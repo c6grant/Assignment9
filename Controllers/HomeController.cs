7495f834b1f97f9b7c2b65dba01015a74ab5f32a
Connor Grant <71059606+c6grant@users.noreply.github.com>
2021-03-25 17:41:10 -0600

    This is Assignemnt 9

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Inputs.Where(r => r.Title != "Independence Day"));
+        }
+
+     
+        
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieInput()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieInput(InputResponse appResponse)
+        {
+
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddInput(appResponse);
+                return View("Confirmation", appResponse);
+            }
+            
+            return View("MovieInput", appResponse);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class TempStorage
+    {
+        private static List<InputResponse> inputs = new List<InputResponse>();
+
+        public static IEnumerable<InputResponse> Inputs => inputs;
+
+        public static void AddInput(InputResponse input)
+        {
+            inputs.Add(input);
+        }
+    }
+}
